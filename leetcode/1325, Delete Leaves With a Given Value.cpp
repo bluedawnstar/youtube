5e92f9d128678e1339b89e380c0eec6e0dd8ac45
youngman <youngman.ro@gmail.com>
2020-01-21 03:10:42 +0900

    [Add] 1325. Delete Leaves With a Given Value

---
+++ leetcode/1325, Delete Leaves With a Given Value.cpp
@@ -0,0 +1,48 @@
+/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+// method #1
+class Solution {
+public:
+    TreeNode * dfs(TreeNode* node, int target) {
+        if (node == nullptr)
+            return nullptr;
+
+        node->left = dfs(node->left, target);
+        node->right = dfs(node->right, target);
+
+        if (node->left == nullptr && node->right == nullptr && node->val == target) {
+            //delete node;
+            return nullptr;
+        } else
+            return node;
+    }
+
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        return dfs(root, target);
+    }
+};
+
+// method #2
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* node, int target) {
+        if (node == nullptr)
+            return nullptr;
+
+        node->left = removeLeafNodes(node->left, target);
+        node->right = removeLeafNodes(node->right, target);
+
+        if (node->left == nullptr && node->right == nullptr && node->val == target) {
+            //delete node;
+            return nullptr;
+        } else
+            return node;
+    }
+};
